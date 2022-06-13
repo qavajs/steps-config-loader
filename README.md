0cd5d47ee2f679e6f090179e42beccf9ccacfc62
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-06-13 11:52:24 +0300

    rewrite module to ts. update dependencies added readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# steps-config-loader
-config loader for @cucumber-e2e project
+# @yaatp/steps-config-loader
+This is a core package to load global config and memory object. Must be a first dependency in require property
+
+```javascript
+const Memory = require('./memory');
+module.exports = {
+    default: {
+        require: [
+            '@yaatp/steps-config-loader'
+        ],
+        memory: new Memory()
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Before, After, defineParameterType } from '@cucumber/cucumber';
+import path from 'path';
+import memory from '@yaatp/memory';
+
+declare global {
+    let config: any;
+}
+
+defineParameterType({
+    name: 'memory',
+    regexp: /'(.+)'/,
+    transformer: p => memory.getValue(p)
+});
+
+Before(function () {
+    const configPath = process.env.CONFIG as string;
+    const profile = process.env.PROFILE as string;
+    config = require(path.join(process.cwd(), configPath))[profile];
+});
+
+Before(function () {
+    memory.register(config.memory ?? {});
+});
